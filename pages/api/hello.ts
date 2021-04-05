d4122e56e057cf7fdb3f3a0ebb52374256b9d57d
Dilven <mateusiakkrystian@gmail.com>
2021-04-05 20:57:26 +0200

    Add method to handler requests in api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,8 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from "next";
+import { methodHandler } from "@helpers/method-handler";
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+const getHandler = async (_req: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json(JSON.stringify({ message: 'hello' }));
+};
+
+export default methodHandler({ get: getHandler });
